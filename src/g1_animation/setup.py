08cdd29246ff95d201db7ef32ce2fa5548efca1a
Nacho <ignacio.rosa.studio@gmail.com>
2026-03-26 13:07:24 -0300

    Fix capture tool

--- src/g1_animation/setup.py
+++ src/g1_animation/setup.py
@@ -26,6 +26,7 @@ setup(
             'animation_publisher = g1_animation.animation_publisher:main',
             'animation_cli       = g1_animation.animation_cli:main',
             'robot_publisher     = g1_animation.robot_publisher:main',
+            'pose_capture = g1_animation.pose_capture:main',
         ],
     },
 )
